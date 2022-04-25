3f0563fa508c0dd5032fcb903261334acc8f38ce
Vinod.dangi <Vinod.dangi@gwl.com>
2022-04-25 07:39:26 +0530

    Update count with handler

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,10 @@ import { useState } from "react"
 export default function Home() {
   const [count, setCount] = useState(0)
 
-  return <div>Showing total count : {count} </div>
+  return (
+    <>
+      <div>Showing total count : {count} </div>
+      <button onClick={() => setCount(count + 1)}>Add Count</button>
+    </>
+  )
 }
